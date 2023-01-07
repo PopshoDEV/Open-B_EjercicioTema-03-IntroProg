327eee34aa69bda057543f351aa878cea858db17
Robert M. Charpantier <82615587+PopshoDEV@users.noreply.github.com>
2023-01-07 11:32:42 -0600

    Resolving Practice inheritance and polymorphism

--- src/TOB04/Pract_Here_Poli.java
+++ src/TOB04/Pract_Here_Poli.java
@@ -4,8 +4,33 @@ public class Pract_Here_Poli {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Cliente cliente = new Cliente();	
+		//OJO
 		
+		Trabajador trabajador = new Trabajador();
+		
+		trabajador.salario = 1500.42;
+		System.out.println(trabajador.salario);
 	}
+}
 
+class Persona {
+	
+	public String elNombre;
+	public int laEdad;
+	public float elTelefono;
+}
+
+class Cliente extends Persona{
+	public double credito;	
+	public void propiedades(String elNombre, int laEdad, float elTelefono, double credito) {
+		this.elNombre = "Robert M. Charpantier";
+		this.laEdad=19;
+		this.elTelefono=8854833;
+		credito=2.15;
+	}
+} 
+
+class Trabajador extends Persona{
+	public double salario;
 }
